5cc716a2926b2c083e8b714dbe7593d717479ceb
costantino03 <admin@costantino03.net>
2025-05-06 21:40:51 +0200

    modifiche al run

--- new-keycloak/run.sh
+++ new-keycloak/run.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
-# Carica i valori definiti dall'utente
-DB_HOST=${DB_HOST:-localhost}
-DB_DATABASE=${DB_DATABASE:-keycloak}
-DB_USERNAME=${DB_USERNAME:-keycloak}
-DB_PASSWORD=${DB_PASSWORD:-keycloak}
-HOSTNAME=${HOSTNAME:-yourdomain.com}
+# Carica i valori definiti dall'utente tramite Home Assistant
+DB_HOST="${DB_HOST:-$(bashio::config 'db_host')}"
+DB_DATABASE="${DB_DATABASE:-$(bashio::config 'db_database')}"
+DB_USERNAME="${DB_USERNAME:-$(bashio::config 'db_username')}"
+DB_PASSWORD="${DB_PASSWORD:-$(bashio::config 'db_password')}"
+HOSTNAME="${HOSTNAME:-$(bashio::config 'hostname')}"
 
 # Imposta le variabili d'ambiente per Keycloak
-export KC_DB=postgres
-export KC_DB_URL_HOST=${DB_HOST}
-export KC_DB_URL_DATABASE=${DB_DATABASE}
-export KC_DB_USERNAME=${DB_USERNAME}
-export KC_DB_PASSWORD=${DB_PASSWORD}
-export KC_HOSTNAME=${HOSTNAME}
+export KC_DB=mysql
+export KC_DB_URL_HOST="${DB_HOST}"
+export KC_DB_URL_DATABASE="${DB_DATABASE}"
+export KC_DB_USERNAME="${DB_USERNAME}"
+export KC_DB_PASSWORD="${DB_PASSWORD}"
+export KC_HOSTNAME="${HOSTNAME}"
 export KC_PROXY=edge
 
 # Avvia Keycloak in modalità production
-echo "Avvio del server Keycloak in modalità production con la seguente configurazione:"
-echo "Database Host: ${DB_HOST}"
-echo "Database Name: ${DB_DATABASE}"
-echo "Database Username: ${DB_USERNAME}"
-echo "Hostname: ${HOSTNAME}"
+echo "Avvio del server Keycloak con la seguente configurazione:"
+echo "Database Host: ${KC_DB_URL_HOST}"
+echo "Database Name: ${KC_DB_URL_DATABASE}"
+echo "Database Username: ${KC_DB_USERNAME}"
+echo "Hostname: ${KC_HOSTNAME}"
 
 exec /opt/keycloak/bin/kc.sh start-dev
