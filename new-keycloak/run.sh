5d31fe664d131741ce520acd88f85e9ac6487265
costantino03 <admin@costantino03.net>
2025-05-07 18:58:37 +0200

    nuovo tentativo

--- new-keycloak/run.sh
+++ new-keycloak/run.sh
@@ -7,7 +7,8 @@ export DB_USER=$(jq -r '.db_username' /data/options.json)
 export DB_PASSWORD=$(jq -r '.db_password' /data/options.json)
 export HOSTNAME=$(jq -r '.hostname' /data/options.json)
 export PORT=$(jq -r '.port' /data/options.json)
-
+export KEYCLOAK_ADMIN=$(jq -r '.admin_user' /data/options.json)
+export KEYCLOAK_ADMIN_PASSWORD=$(jq -r '.admin_password' /data/options.json)
 # Imposta variabili Keycloak per MySQL
 export KC_DB=mysql
 export KC_DB_USERNAME="$DB_USER"
@@ -15,10 +16,11 @@ export KC_DB_PASSWORD="$DB_PASSWORD"
 export KC_DB_URL="jdbc:mysql://${DB_HOST}:3306/${DB_NAME}?useSSL=false&allowPublicKeyRetrieval=true"
 
 # Credenziali admin (hardcoded, puoi anche renderle configurabili)
-export KEYCLOAK_ADMIN=admin
-export KEYCLOAK_ADMIN_PASSWORD=admin
+# export KEYCLOAK_ADMIN=admin
+# export KEYCLOAK_ADMIN_PASSWORD=admin
 
-echo "Starting Keycloak on http://$HOSTNAME:$PORT using MySQL at $DB_HOST"
+
+echo "Avvio Keycloak su http://$HOSTNAME:$PORT con MySQL"
 
 # Avvia Keycloak
 exec kc.sh start \
